1eba68e0779083659ab010f4356b8b1ea7310ece
phpusr <phpusr@gmail.com>
2014-05-13 12:23:26 +0600

    improve: Внешний вид формы

--- src/main/scala/km/lab/two/form/main/MainForm.scala
+++ src/main/scala/km/lab/two/form/main/MainForm.scala
@@ -1,9 +1,10 @@
 package km.lab.two.form.main
 
-import scala.swing.{Label, GridBagPanel, MainFrame, SimpleSwingApplication}
-import java.awt.{Insets, Dimension}
+import scala.swing._
+import java.awt.Insets
 import km.lab.two.workshop.Workshop
 import km.lab.two.constants.Const
+import javax.swing.BorderFactory
 
 /**
  * @author phpusr
@@ -28,7 +29,9 @@ object MainForm extends SimpleSwingApplication {
   private val avgDetailHandlerTimeV2Label = defaultLabel()
 
   // Очередь в станках
-  private val machineToolsQueueSizeList = for (i <- 1 to 3) yield defaultLabel()
+  private val machineToolsQueueSizeList = for (i <- 1 to 3) yield new Label("0") {
+    tooltip = "В очереди (обрабатывается) (средняя загрузка)"
+  }
 
   /** Время работы мастерской */
   private val workedTimeLabel = defaultLabel()
@@ -39,41 +42,60 @@ object MainForm extends SimpleSwingApplication {
   def top = new MainFrame {
     contents = new GridBagPanel {
       val c = new Constraints
-      c.insets = new Insets(5, 5, 5, 5)
+      c.insets = new Insets(5, 20, 5, 20)
 
-      layout(new Label("Detail queue:")) = c
-      layout(detailQueueSizeLabel) = c
+      layout(new FlowPanel {
+        contents += new Label("Отработано времени:")
+        contents += workedTimeLabel
+      }) = c
 
       c.gridy = 1
-      layout(new Label("All generate detail:")) = c
-      layout(generateDetailCountLabel) = c
+      layout(new GridBagPanel {
+        border = BorderFactory.createTitledBorder("Станки")
+        val c = new Constraints
+        c.insets = new Insets(5, 5, 5, 5)
 
-      c.gridy = 2
-      layout(new Label("Warehouse:")) = c
-      layout(warehouuseDetailCountLabel) = c
+        for (i <- 0 until machineToolsQueueSizeList.size) {
+          c.gridy = i
+          layout(new Label(s"A${i+1}:")) = c
+          layout(machineToolsQueueSizeList(i)) = c
+        }
+      }) = c
 
-      c.gridy = 3
-      layout(new Label("Avg detail V1 handler time:")) = c
-      layout(avgDetailHandlerTimeV1Label) = c
+      layout(new GridBagPanel {
+        border = BorderFactory.createTitledBorder("Статистика деталей")
+        val c = new Constraints
+        c.insets = new Insets(5, 5, 5, 5)
 
-      c.gridy = 4
-      layout(new Label("Avg detail V2 handler time:")) = c
-      layout(avgDetailHandlerTimeV2Label) = c
+        layout(new Label("Всего поступивших:")) = c
+        layout(generateDetailCountLabel) = c
 
-      c.gridy = 5
-      layout(new Label("Worked time")) = c
-      layout(workedTimeLabel) = c
+        c.gridy = 1
+        layout(new Label("Обработанные (на складе):")) = c
+        layout(warehouuseDetailCountLabel) = c
 
-      for (i <- 0 until machineToolsQueueSizeList.size) {
-        c.gridx = 2
-        c.gridy = i
-        layout(new Label(s"A${i+1}:")) = c
-        c.gridx = 3
-        layout(machineToolsQueueSizeList(i)) = c
-      }
+        c.gridy = 2
+        layout(new Label("Очередь:")) = c
+        layout(detailQueueSizeLabel) = c
+
+      }) = c
+
+      layout(new GridBagPanel {
+        border = BorderFactory.createTitledBorder("Среднее время обработки детали")
+        val c = new Constraints
+        c.insets = new Insets(5, 5, 5, 5)
+
+        layout(new Label("Деталь 1-о типа:")) = c
+        layout(avgDetailHandlerTimeV1Label) = c
+
+        c.gridy = 1
+        layout(new Label("Деталь 2-о типа:")) = c
+        layout(avgDetailHandlerTimeV2Label) = c
+
+      }) = c
+
     }
 
-    size = new Dimension(500, 300)
     centerOnScreen()
   }
 
@@ -106,9 +128,9 @@ object MainForm extends SimpleSwingApplication {
   }).start()
 
   /** мс. -> мин. (с учетом ускорения времени) */
-  private def milisToMinutes(milis: Long) = Math.round(milis.toFloat * Const.Acceleration / 1000 / 60).toString + " min"
+  private def milisToMinutes(milis: Long) = Math.round(milis.toFloat * Const.Acceleration / 1000 / 60).toString + " мин."
 
   /** мс. -> ч. (с учетом ускорения времени) */
-  private def milisToHours(milis: Long) = Math.round(milis.toFloat * Const.Acceleration / 1000 / 60 / 60).toString + " h"
+  private def milisToHours(milis: Long) = Math.round(milis.toFloat * Const.Acceleration / 1000 / 60 / 60).toString + " ч."
 
 }

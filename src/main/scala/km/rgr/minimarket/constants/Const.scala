b9a454c5b6188500724eb8f4e22e361d1f949e7d
phpusr <phpusr@gmail.com>
2014-05-15 14:45:11 +0600

    add: TODO

---
+++ src/main/scala/km/rgr/minimarket/constants/Const.scala
@@ -0,0 +1,20 @@
+package km.rgr.minimarket.constants
+
+/**
+ * @author phpusr
+ *         Date: 12.05.14
+ *         Time: 13:28
+ */
+
+/**
+ * Константы
+ */
+object Const {
+
+  /** Ускорение времени */ //TODO
+  val Acceleration = 6000
+
+  /** Время засыпания потоков */
+  val ThreadSleepMilis = 100
+
+}

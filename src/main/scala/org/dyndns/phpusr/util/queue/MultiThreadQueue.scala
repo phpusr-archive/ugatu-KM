b1bfbbce51ef6e816a182eef1551ee3dca3c439f
phpusr <phpusr@gmail.com>
2014-05-15 14:44:49 +0600

    add: MultiThreadQueue

---
+++ src/main/scala/org/dyndns/phpusr/util/queue/MultiThreadQueue.scala
@@ -0,0 +1,24 @@
+package org.dyndns.phpusr.util.queue
+
+import scala.collection.mutable
+
+/**
+ * @author phpusr
+ *         Date: 15.05.14
+ *         Time: 14:07
+ */
+
+/**
+ * Потокобезопасная очередь
+ */
+class MultiThreadQueue[A] {
+
+  private val queue = mutable.Queue[A]()
+
+  def dequeue() = synchronized(queue.dequeue())
+
+  def +=(el: A) = synchronized(queue += el)
+
+  def nonEmpty = queue.nonEmpty
+
+}

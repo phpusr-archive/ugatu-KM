a1d816743e01d5a0ac9252cbda720362726730d1
phpusr <phpusr@gmail.com>
2014-05-15 19:15:05 +0600

    add: Время ожидания обслуживания в Customer

--- src/main/scala/km/rgr/minimarket/Customer.scala
+++ src/main/scala/km/rgr/minimarket/Customer.scala
@@ -1,6 +1,6 @@
 package km.rgr.minimarket
 
-import java.util.{TimerTask, Timer}
+import java.util.{Date, TimerTask, Timer}
 import km.rgr.minimarket.constants.Const
 
 /**
@@ -19,6 +19,12 @@ case class Customer(name: String) {
   
   private var _allBought = false
 
+  /** Время становления в очередь */
+  private var _startWaitTime: Date = null
+
+  /** Время ожидания обслуживания (мс.) */
+  private var _waitMillis = 0L
+
   // Таймер покупок покупателя
   new Timer().schedule(new TimerTask {
     override def run() {
@@ -28,5 +34,23 @@ case class Customer(name: String) {
 
   /** Все ли купил покупатель (идет в очередь) */
   def allBought = _allBought
+
+  /** Покупатель встал в очередь ожидания обслуживания */
+  def startWait() = _startWaitTime = new Date
+
+  /** Подсчитать время ожидания обслуживания */
+  def calcWait() = {
+    assert(_startWaitTime != null)
+    _waitMillis = new Date().getTime - _startWaitTime.getTime
+    waitMillis
+  }
+
+  /** Время ожидания обслуживания (мс.) */
+  def waitMillis = {
+    assert(_startWaitTime != null)
+    assert(_waitMillis != 0)
+    _waitMillis
+  }
+
 
 }
